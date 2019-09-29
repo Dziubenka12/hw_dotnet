bf954e92a678168e4239157262c10cd4c0f6a111
Dziubenko <dziubenka12@gmail.com>
2019-09-29 23:04:13 +0300

    whole homework

---
+++ ItAcademyHomeWorks/HW_03_Task1/HW_03_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_03_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+        }
+        static void Sum()
+        {
+            Console.WriteLine("Введите первое число");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine($"Сумма равна {sum}");
+        }
+}
+}
