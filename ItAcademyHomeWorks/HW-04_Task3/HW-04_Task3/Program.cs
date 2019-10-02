3125c9170351c9f8b344b62c663d4cc4471c7910
Dziubenko <dziubenka12@gmail.com>
2019-10-02 22:08:13 +0300

    Task3

--- ItAcademyHomeWorks/HW-04_Task3/HW-04_Task3/Program.cs
+++ ItAcademyHomeWorks/HW-04_Task3/HW-04_Task3/Program.cs
@@ -6,7 +6,18 @@ namespace HW_04_Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float vklad = 1000;
+            float proc;
+            for (int i = 0; i < 12; i++)
+            {
+                proc = vklad * (float)0.02;
+                vklad += vklad * (float)0.02;
+                Console.WriteLine($"Прирост суммы {proc}");
+                if (i > 1 && i < 12)
+                {
+                    Console.WriteLine($"Сумма вклада {vklad}");
+                }
+            }
         }
     }
 }
