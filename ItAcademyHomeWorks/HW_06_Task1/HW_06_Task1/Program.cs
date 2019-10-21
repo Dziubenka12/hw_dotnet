e4c9c0f15c7460d391c895edc04b94d5058d7dcf
Dziubenko Anatoli <49936231+Dziubenka12@users.noreply.github.com>
2019-10-21 13:34:15 +0300

    HW6

---
+++ ItAcademyHomeWorks/HW_06_Task1/HW_06_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HW_06_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение!");
+            string stih = Console.ReadLine();
+            char[] ar = stih.ToCharArray();
+            for (int i = 0; i < stih.Length; i++)
+            {
+                if (stih[i] == (char)1054)
+                {
+                    ar[i] = (char)1040;
+                }
+                else if (stih[i] == (char)1086)
+                {
+                    ar[i] = (char)1072;
+                }
+            }
+            stih = String.Concat<char>(ar);
+            string[] array = stih.Split(";");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/HW_06_Task2/HW_06_Task2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace HW_06_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Дана строка символов. Слова в строке отделяются друг от друга одним пробелом. Удалить из строки самое длинное слово.");
+            //LongWord();
+            //Lsword();
+            //Kalk();
+            Down();
+            Console.ReadKey();
+        }
+        static void LongWord()
+        {
+            string str = "Дана строка символов. Слова в строке отделяются друг от друга одним пробелом. Удалить из строки самое длинное слово.";
+            string[] longW = str.Split(' ');
+            //int temp = 0;
+            int max = longW[0].Length;
+
+            for (int i = 0; i < longW.Length; i++)
+                if (max < longW[i].Length)
+                {
+                    max = longW[i].Length;
+                    //temp = i; 
+                }
+            for (int i = 0; i < longW.Length; i++)
+                if (max == longW[i].Length)
+                {
+                    Console.WriteLine(longW[i]);
+                }
+        }
+
+        static void Lsword()
+        {
+            string str = "Дана строка символов. Слова в строке отделяются друг от друга одним пробелом. Удалить из строки самое длинное слово.";
+            string[] longW = str.Split(' ');
+            int Llong = 0;
+            int Lshort = 0;
+            string s;
+            int max = longW[0].Length;
+
+            for (int i = 0; i < longW.Length; i++)
+                if (max < longW[i].Length)
+                {
+                    max = longW[i].Length;
+                    Llong = i;
+                }
+            for (int i = 0; i < longW.Length; i++)
+                if (max > longW[i].Length)
+                {
+                    max = longW[i].Length;
+                    Lshort = i;
+                }
+            s = longW[Llong];
+            longW[Llong] = longW[Lshort];
+            longW[Lshort] = s;
+            for (int i = 0; i < longW.Length; i++)
+            {
+                Console.Write($"{longW[i]} ");
+            }
+        }
+
+        static void Kalk()
+        {
+            string str = "Дана строка символов. Слова в строке отделяются друг от друга одним пробелом. Посчитать сколько в строке букв, пунктуационных знаков.";
+            Console.WriteLine($"Количество пунктуационных знаков {str.Count(char.IsPunctuation)}");
+            Console.WriteLine($"Количество букв {str.Count(char.IsLetter)}");
+        }
+
+        static void Down()
+        {
+            string str = "Дана строка символов. Слова в строке отделяются друг от друга одним пробелом. Удалить из строки самое длинное слово.";
+            string[] array = str.Split(" ");
+            Array.Sort(array, (x, y) => x.Length.CompareTo(y.Length));
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
